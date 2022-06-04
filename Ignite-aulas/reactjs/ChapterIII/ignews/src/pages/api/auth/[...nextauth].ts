b356274219e25d05ae735ca9e948d9b46195e8e5
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-06-03 23:25:36 -0300

    feat: implement integration faunaDB.

--- Ignite-aulas/reactjs/ChapterIII/ignews/src/pages/api/auth/[...nextauth].ts
+++ Ignite-aulas/reactjs/ChapterIII/ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb';
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+
+import { fauna } from '../../../services/fauna';
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -7,6 +11,34 @@ export default NextAuth({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email as string),
+                ),
+              ),
+            ),
+            q.Create(q.Collection('users'), { data: { email } }),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email as string),
+              ),
+            ),
+          ),
+        );
+        return true;
+      } catch (error) {
+        return false;
+      }
+    },
+  },
 });
