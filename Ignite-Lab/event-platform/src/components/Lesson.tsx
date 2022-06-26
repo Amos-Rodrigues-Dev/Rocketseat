a6a4a24c8df9f455259cd2efcdf6a27a50a842e8
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-06-26 16:34:46 -0300

    feat: implement grapql in project react.

--- Ignite-Lab/event-platform/src/components/Lesson.tsx
+++ Ignite-Lab/event-platform/src/components/Lesson.tsx
@@ -1,7 +1,8 @@
 import { CheckCircle, Lock } from 'phosphor-react';
 import { isPast, format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import classNames from 'classnames';
 
 interface LessonProps {
   title: string;
@@ -11,6 +12,8 @@ interface LessonProps {
 }
 
 export function Lesson({ title, availableAt, lessonType, slug }: LessonProps) {
+  const { slug: slugParams } = useParams<{ slug: string }>();
+
   const isLessonAvailable = isPast(availableAt);
   const availableDateFormatted = format(
     availableAt,
@@ -18,14 +21,29 @@ export function Lesson({ title, availableAt, lessonType, slug }: LessonProps) {
     { locale: ptBR },
   );
 
+  const isActiveLesson = slugParams === slug;
+
   return (
     <Link to={`/event/lesson/${slug}`} className="group">
       <span className="text-gray-300">{availableDateFormatted}</span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 group-hover:scale-105 transition-transform duration-300">
+      <div
+        className={classNames(
+          'rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 group-hover:scale-105 transition-transform duration-300',
+          {
+            'bg-green-500': isActiveLesson,
+          },
+        )}>
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
-            <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+            <span
+              className={classNames(
+                'text-sm font-medium flex items-center gap-2',
+                {
+                  'text-white': isActiveLesson,
+                  'text-blue-500': !isActiveLesson,
+                },
+              )}>
               <CheckCircle size={20} />
               Conteúdo liberado
             </span>
@@ -36,12 +54,25 @@ export function Lesson({ title, availableAt, lessonType, slug }: LessonProps) {
             </span>
           )}
 
-          <span className="text-xs rounded py-[0.125rem] px-2 text-white border border-green-300 font-bold">
+          <span
+            className={classNames(
+              'text-xs rounded py-[0.125rem] px-2 text-white border font-bold',
+              {
+                'border-white': isActiveLesson,
+                'border-green-300': !isActiveLesson,
+              },
+            )}>
             {lessonType === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
           </span>
         </header>
 
-        <strong className="text-gray-200 mt-5 block">{title}</strong>
+        <strong
+          className={classNames('mt-5 block', {
+            'text-white': isActiveLesson,
+            'text-gray-200': !isActiveLesson,
+          })}>
+          {title}
+        </strong>
       </div>
     </Link>
   );
