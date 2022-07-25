b9b157497901fd3fbbc4f104c437b96715e49e69
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-07-24 23:44:20 -0300

    feat: implement cycles structure the of tasks.

--- Ignite-aulas/ReactJS/ChapterII/02-ignite-timer/src/pages/Home/index.tsx
+++ Ignite-aulas/ReactJS/ChapterII/02-ignite-timer/src/pages/Home/index.tsx
@@ -12,6 +12,7 @@ import {
   StartCountDownButton,
   TaskInput,
 } from './styles'
+import { useState } from 'react'
 
 // controlled - manter informações em tempo real dentro do estado
 // uncontrolled - buscar informações somente quando necessário
@@ -34,7 +35,16 @@ const newCycleFormValidationSchema = zod.object({
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
+interface Cycle {
+  id: string
+  task: string
+  minutesAmount: number
+}
+
 export function Home() {
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
@@ -44,9 +54,22 @@ export function Home() {
   })
 
   const handleCreateNewCycle = (data: NewCycleFormData) => {
-    console.log(data)
+    const id = String(new Date().getTime())
+
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+    }
+
+    setCycles((state) => [...state, newCycle])
+    setActiveCycleId(id)
     reset()
   }
+
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  console.log(activeCycle)
 
   const task = watch('task')
   const isSubmitDisabled = !task
