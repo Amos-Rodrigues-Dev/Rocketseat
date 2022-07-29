3aee30edce47158e2d9cbd60fb2116a06dc4e1ea
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-07-29 19:33:57 -0300

    feat: implement cycle interrupt.

--- Ignite-aulas/ReactJS/ChapterII/02-ignite-timer/src/pages/Home/index.tsx
+++ Ignite-aulas/ReactJS/ChapterII/02-ignite-timer/src/pages/Home/index.tsx
@@ -59,12 +59,18 @@ export function Home() {
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   useEffect(() => {
+    let interval: number
+
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate),
         )
       }, 1000)
+    }
+
+    return () => {
+      clearInterval(interval)
     }
   }, [activeCycle])
 
@@ -80,6 +86,8 @@ export function Home() {
 
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
+    setAmountSecondsPassed(0)
+
     reset()
   }
 
@@ -91,6 +99,12 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [activeCycle, minutes, seconds])
 
   const task = watch('task')
   const isSubmitDisabled = !task
@@ -110,8 +124,7 @@ export function Home() {
           <datalist id="task-suggestions">
             <option value="Projeto 1" />
             <option value="Projeto 2" />
-            <option value="Projeto 1" />
-            <option value="banana" />
+            <option value="Projeto 3" />
           </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
