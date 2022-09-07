b976b09617a41f2ae4b5d01137ece4176997bc1f
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-09-07 09:50:26 -0300

    feat: configure checkout session.

--- Ignite-aulas/reactjs/ChapterIII/ignews/src/pages/api/subscribe.ts
+++ Ignite-aulas/reactjs/ChapterIII/ignews/src/pages/api/subscribe.ts
@@ -18,7 +18,8 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     const session = await getSession({ req });
     // const { data: session } = useSession();
-
+    console.log('aqui');
+    
     const user = await fauna.query<User>(
       q.Get(
         q.Match(q.Index('user_by_email'), q.Casefold(session?.user?.email!)),
