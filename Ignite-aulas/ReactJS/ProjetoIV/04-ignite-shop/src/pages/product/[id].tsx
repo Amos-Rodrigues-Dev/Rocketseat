3562e6ff14ae21861f6f490b444be70dfbe0fadd
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-09-11 18:56:45 -0300

    feat: implement style of the product.

--- Ignite-aulas/ReactJS/ProjetoIV/04-ignite-shop/src/pages/product/[id].tsx
+++ Ignite-aulas/ReactJS/ProjetoIV/04-ignite-shop/src/pages/product/[id].tsx
@@ -1,11 +1,30 @@
 import { useRouter } from 'next/router';
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '../../styles/pages/product';
 
 export default function ProductItem() {
   const { query } = useRouter();
 
   return (
-    <div>
-      <h1>Product Item {JSON.stringify(query)}</h1>
-    </div>
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In vitae ut
+          dolor repudiandae ab vel debitis voluptates consectetur sit atque
+          voluptatum voluptas nam, omnis sint, nobis maiores, expedita adipisci
+          explicabo.
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
   );
 }
