9d365863a64896783a7b9cb1be5b04bba3f957f8
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-10-04 16:34:12 -0300

    feat: init project from ChapterII.

--- Ignite-aulas/nodejs/ChapterII/src/server.ts
+++ Ignite-aulas/nodejs/ChapterII/src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import { createCourse } from './routes';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 const PORT = 3333;
@@ -9,6 +9,6 @@ const PORT = 3333;
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', createCourse);
+app.use(categoriesRoutes);
 
-app.listen(3333, () => console.log(`Rodando na porta ${PORT}`));
+app.listen(3333, () => console.log(`Server is running in port ${PORT}`));
