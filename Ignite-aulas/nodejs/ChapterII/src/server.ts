e1fa0fc32f908725cb48baa246f9bd1727fd7c89
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-10-12 16:27:02 -0300

    feat: create repository especification.

--- Ignite-aulas/nodejs/ChapterII/src/server.ts
+++ Ignite-aulas/nodejs/ChapterII/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import { categoriesRoutes } from './routes/categories.routes';
+import { specificationsRoutes } from './routes/specification.routes';
 
 const app = express();
 const PORT = 3333;
@@ -10,5 +11,6 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/categories', categoriesRoutes);
+app.use('/specifications', specificationsRoutes);
 
 app.listen(3333, () => console.log(`Server is running in port ${PORT}`));
