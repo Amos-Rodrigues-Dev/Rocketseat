28de674811a635953b70f9e103c7253b9a1134f4
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-10-15 19:23:19 -0300

    feat: implement swagger.

--- Ignite-aulas/nodejs/ChapterII/src/server.ts
+++ Ignite-aulas/nodejs/ChapterII/src/server.ts
@@ -1,13 +1,19 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+
 import bodyParser from 'body-parser';
 import cors from 'cors';
+
 import { router } from './routes';
+import swaggerFile from './swagger.json';
 
 const app = express();
 const PORT = 3333;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
