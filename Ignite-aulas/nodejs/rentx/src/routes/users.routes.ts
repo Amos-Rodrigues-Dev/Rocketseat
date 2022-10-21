e115335341a38afb5fe17a2ba8d73837b9977a80
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-10-21 11:46:19 -0300

    nodejs/feat: implement autheticate with jsonwebtoken.

--- Ignite-aulas/nodejs/rentx/src/routes/users.routes.ts
+++ Ignite-aulas/nodejs/rentx/src/routes/users.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import { CreateUserController } from '../modules/accounts/useCases/createUser/CreateUserController';
 
-const usersRouter = Router();
+const usersRoutes = Router();
 
 const createUserUseCase = new CreateUserController();
 
-usersRouter.post('/', createUserUseCase.handle);
+usersRoutes.post('/', createUserUseCase.handle);
 
-export { usersRouter };
+export { usersRoutes };
