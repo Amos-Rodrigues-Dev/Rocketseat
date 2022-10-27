43b62bd3640dc14cb3b42f839ef705793510f1cb
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-10-26 22:38:34 -0300

    reactjs/feat: implement components Box and Text.

---
+++ Ignite-aulas/ReactJS/ProjetoV/05-design-system/packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus totam voluptatibus cupiditate dolorem vero, a, suscipit reiciendis hic aut maiores illum, ad est impedit ullam voluptates perspiciatis sunt fuga nihil!',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
