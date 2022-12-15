5d66e1e700df3e046bc0cb115c3b3e7917adae23
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-12-15 17:20:27 -0300

    fix: ajust mappers include field canceledAt.

--- Ignite-Lab/notifications-service/src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ Ignite-Lab/notifications-service/src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -6,10 +6,11 @@ export class PrismaNotificationMapper {
   static toPrisma(notification: Notification) {
     return {
       id: notification.id,
-      category: notification.category,
       content: notification.content.value,
+      category: notification.category,
       recipientId: notification.recipientId,
       readAt: notification.readAt,
+      canceledAt: notification.canceledAt,
       createdAt: notification.createdAt,
     };
   }
