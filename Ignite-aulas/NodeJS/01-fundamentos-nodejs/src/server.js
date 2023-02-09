8db2ccdb5a646ffd78ed1b1c1f2939ef6e431295
Amós Rodrigues <amos.adm.rh@gmail.com>
2023-02-09 18:24:06 -0300

    node/feat: implement route delete user.

--- Ignite-aulas/NodeJS/01-fundamentos-nodejs/src/server.js
+++ Ignite-aulas/NodeJS/01-fundamentos-nodejs/src/server.js
@@ -14,16 +14,30 @@ import { routes } from './routes.js';
 
 // Cabeçalhos (requisição/resposta) => metadados
 
+// Query Parameters (Parametros nomeados): URL Stateful => filtros, paginações, não-obrigatórias
+// ex: http://localhost:3333/users?userId=1&name=Amos
+
+// Route Parameters (Parâmetros não nomeados): Identificação de recurso
+// ex: http://localhost:3333/users/1
+
+// Request Body: Envio de informações de um formulário
+
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
   await json(req, res);
 
   const route = routes.find(
-    (route) => route.method === method && route.path === url,
+    (route) => route.method === method && route.path.test(url),
   );
 
-  if (route) return route.handler(req, res);
+  if (route) {
+    const routeParams = req.url.match(route.path)
+
+    req.params = {...routeParams.groups}
+
+    return route.handler(req, res);
+  }
 
   return res.writeHead(404).end('Not found');
 });
