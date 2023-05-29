420b2935d133f63c3201a86e6273a0cfa123fe0b
Amós Rodrigues <amos.adm.rh@gmail.com>
2023-05-29 15:43:55 -0300

    node/feat: implement use case delete question.

---
+++ Ignite-aulas/NodeJS/04-clean-ddd/src/domain/forum/application/use-cases/delete-question.spec.ts
@@ -0,0 +1,47 @@
+import { expect } from 'vitest'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { makeQuestion } from 'test/factories/make-question'
+import { DeleteQuestionUseCase } from './delete-question'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: DeleteQuestionUseCase // system under test
+
+describe('Delete Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new DeleteQuestionUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('should be able to deleted a question', async () => {
+    const newQuestion = await makeQuestion(
+      { authorId: new UniqueEntityId('author-1') },
+      new UniqueEntityId('question-1'),
+    )
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    await sut.execute({
+      questionId: 'question-1',
+      authorId: 'author-1',
+    })
+
+    expect(inMemoryQuestionsRepository.items).toHaveLength(0)
+  })
+
+  it('should not be able to deleted a question from another user', async () => {
+    const newQuestion = await makeQuestion(
+      { authorId: new UniqueEntityId('author-1') },
+      new UniqueEntityId('question-1'),
+    )
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    await expect(() => {
+      return sut.execute({
+        questionId: 'question-1',
+        authorId: 'author-2',
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+})
