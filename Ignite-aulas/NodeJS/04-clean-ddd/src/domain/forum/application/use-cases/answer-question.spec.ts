e43496de15ff6407bf3c38078d70552f54b3cd6d
Amós Rodrigues <amos.adm.rh@gmail.com>
2023-05-29 10:43:57 -0300

    node/feat: create question use case.

--- Ignite-aulas/NodeJS/04-clean-ddd/src/domain/forum/application/use-cases/answer-question.spec.ts
+++ Ignite-aulas/NodeJS/04-clean-ddd/src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,16 +1,16 @@
 import { AnswerQuestionUseCase } from './answer-question'
-import { AnswerRepository } from '../repositories/answers-repository'
+import { AnswersRepository } from '../repositories/answers-repository'
 import { Answer } from '@/domain/forum/enterprise/entities/answer'
 import { expect } from 'vitest'
 
-const fakeAnswerRepository: AnswerRepository = {
+const fakeAnswerRepository: AnswersRepository = {
   create: async (answer: Answer) => {},
 }
 
 test('create an answer', async () => {
   const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
 
-  const answer = await answerQuestion.execute({
+  const { answer } = await answerQuestion.execute({
     questionId: '1',
     instructorId: '1',
     content: 'Nova resposta',
